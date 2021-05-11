01c4ca3da8aee8587d8df932f2237daecd0f5ee2
Fawaz Ahmed <fawazahmed0@hotmail.com>
2021-05-11 19:35:07 +0530

    Update setdate.js

--- setdate.js
+++ setdate.js
@@ -1,30 +1,46 @@
-const glob = require("glob");
 const path = require('path')
 const fs = require('fs')
 const indent = 4
 
-var getDirectories = function (src, callback) {
-  glob(src + '/**/*', callback);
-};
 
 
 for (let date of fs.readdirSync( path.join(__dirname, '.') ).filter(e=> /\d{4}-\d{2}-\d{2}/.test(e))){
 
-getDirectories(path.join(__dirname, date,'currencies'), function (err, res) {
-  if (err) {
-    console.log('Error', err);
-  } else {
-    for(let dirval of res.filter(e=>e.endsWith('.json'))){
-        let jsonval = fs.readFileSync(dirval).toString()
-        jsonval = JSON.parse(jsonval)
-        jsonval['date'] = date
-        if (dirval.endsWith('.min.json'))
-        fs.writeFileSync(dirval, JSON.stringify(jsonval))
-        else
-        fs.writeFileSync(dirval, JSON.stringify(jsonval, null, indent))
+for(let file of fs.readdirSync(path.join(__dirname, date,'currencies'))){
+  let filePath = path.join(__dirname, date,'currencies',file)
+  if(fs.statSync(filePath).isDirectory()){
+
+    for(let innerfile of fs.readdirSync(filePath))
+    {
+      let innerfilePath  = path.join(filePath,innerfile)
+      saveDate(innerfilePath, date)
       
-    }      
-  }
-});
+    }
+
+
+
+  }else
+    saveDate(filePath, date)
+  
+ 
+
+}
+
+
+
+}
+
+
+function saveDate(filePath, date){
+  let jsonval = JSON.parse(fs.readFileSync(filePath).toString())
+  let savejson = {};
+  savejson['date'] = date;
+  for(let [key,value] of Object.entries(jsonval))
+  savejson[key]= value
+
+  if (filePath.endsWith('.min.json'))
+  fs.writeFileSync(filePath, JSON.stringify(savejson))
+  else if(filePath.endsWith('.json'))
+  fs.writeFileSync(filePath, JSON.stringify(savejson, null, indent))
 
 }
